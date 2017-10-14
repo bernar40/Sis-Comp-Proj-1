6be8042defba4560eaae5636848f58f73132c7c2
Rodrigo Leite <rodrigo.ldasilva@hotmal.com>
2017-10-14 11:48:20 -0300

    Testa ambiente

--- escalonador.c
+++ escalonador.c
@@ -1,1 +1,14 @@
 //Escalonador
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+
+int main(void){
+	sleep(1);
+	printf("%d",getpid());
+	sleep(5);
+	exit(0);	
+}
