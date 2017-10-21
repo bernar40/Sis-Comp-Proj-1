38500d78d8d08bb12b7d425587f2962d86b8fee2
Bernardo Ruga <bernardo.ruga@hotmail.com>
2017-10-21 15:57:37 -0200

    Interpretador funcionando
    Agora o interpretador executa e fica esperando um stdin do teclado que tem o formato do trabalho:
    Ele separa as palavras em exec / nomedoprograma/ (TEMPO)
    depois cada tempo é atribuido a um elemento do array exect

--- interpretador.c
+++ interpretador.c
@@ -14,24 +14,59 @@ exec <programa8exec81> (2, 10, 4)  // rajadas de 2, 10, 4 segundos, interrompida
 exec <programa8exec81> (20, 20, 2) // rajadas de 20, 20, e 2 segundos, interrompidas por I/O
 */
 
-int main (int argc, char **argv){
+void separador_input(char *input, char word[3][100]){
+    int i= 0, j=0, k=0;
+    input[strlen(input) - 1] = '\0';
+    if (input[0] == '\0') //checa se o input foi vazio
+        printf("Input invalido.\n");
+    while (input[i] != '\0') {
+        if (input[i] == ' ' && k<2) { // ' ' -> primeiro separador
+                word[k][j] = '\0';
+                j = 0;
+                k++;
+        }
+        else {
+                word[k][j++] = input[i];
+        }
+        i++;
+    }
+    word[k][j] = '\0'; //ultima palavra TEMPO
+}
+
+void separador_tempo(char *word, int *exect){
+    char str[100];
+    char tempo_s[3][100];
+    const char s[2] = "(";
+    char *token;
+
+    printf("separador tempo word: %s\n", word);
+    separador_input(word, tempo_s);
+    //printf("t1: %s t2: %s t3: %s\n", tempo_s[0], tempo_s[1], tempo_s[2]);
+    token = strtok(tempo_s[0], s); //remove o parenteses do primeiro tempo.
+    // printf("t1: %s t2: %s t3: %s\n", token, tempo_s[1], tempo_s[2]);
+    exect[0] = atoi(token);
+    exect[1] = atoi(tempo_s[1]);
+    exect[2] = atoi(tempo_s[2]);
+}
+
+int main (void){
+    char input[100];
+    char word[3][100];
     char *str;
     int *exect; //execution time - cada elemento eh um tempo em segundos
-    int i;
-    exect = (int *)malloc((argc - 1)*sizeof(int));
+    exect = (int *)malloc((3*sizeof(int)));
+    while (fgets(input, 100, stdin)) //repete sempre pegando input do teclado
+    {
+        separador_input(input, word);
+        separador_tempo(word[2], exect);
+        //printf("%s nome: %s tempos: %s\n", word[0], word[1], word[2]);
+        //printf("t1: %d -- t2: %d -- t3: %d", exect[0], exect[1], exect[2]);
 
-    for (i=1; i<=argc-1; i++){
-        if (i==1)
-            str = strtok(argv[i],"( ,");
-        else if (i==argc-1)
-            str = strtok(argv[i],")");
-        else
-            str =  strtok(argv[i],",");
-        exect[i-1] = atoi(str);
+        /* CHAMAR O ESCALONADOR AQUI */
     }
-
-    //Chama o escalonador com execv? ou isso eh o main do escalonador.c?
     
-    free(exect);
     return 0;
 }
+
+
+
